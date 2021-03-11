28666bf18c5b504875baf05c582d0aad502a9286
MAliUsuf <78339046+MAliUsuf@users.noreply.github.com>
2021-03-11 04:39:59 -0800

    Test100

--- utils/methods/functions.js
+++ utils/methods/functions.js
@@ -18,7 +18,7 @@ import { database, storageRef, admin } from "../../firebase/firebase";
 export const rightUrl = (extention) => {
     const url = {
         local: "http://localhost:3000/api/admin/",
-        remote: `https://admin8-seven.vercel.app/api/admin/`
+        remote: `https://admin88-alpha.vercel.app/api/admin/`
     }
 
     return process.env.NEXT_PUBLIC_VERCEL_URL ? `${url.remote}${extention}` : `${url.local}${extention} `
